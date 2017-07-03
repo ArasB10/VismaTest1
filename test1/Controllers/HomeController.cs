3908c3a6836e960604358a0daa7f2e83ff8242af
Aras Bražiūnas <aras.braziunas@visma.com>
2017-07-03 16:43:03 +0300

    Add project files.

---
+++ test1/Controllers/HomeController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using test1.Models;
+
+namespace test1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private Contact contact = new Contact();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Name(int id, string typeSet)
+        {
+            /*
+
+            Contact contact = new Contact() {
+                FirstName = "Aras",
+                LastName = "Braziunas",
+                Age = 23,
+            };
+            */
+            
+            List<Contact> list = contact.GetContacts();
+
+            if (typeSet.Equals("up"))
+            {
+                return Content(list.Where(item => item.Id == id).First().FirstName.ToString().ToUpper());
+            }
+            else
+            {
+                return Content(list.Where(item => item.Id == id).First().FirstName.ToString().ToLower());
+            }
+        }
+
+        public ActionResult Age(int id)
+        {
+           
+            List<Contact> list = contact.GetContacts();
+
+            return View(list.Where(item => item.Id == id).First());
+        }
+
+        public ActionResult List()
+        {
+            /*
+
+            Contact contact = new Contact() {
+                FirstName = "Aras",
+                LastName = "Braziunas",
+                Age = 23,
+            };
+            */
+           
+            List<Contact> list = contact.GetContacts();
+
+            return View(list);
+        }
+
+        public ActionResult Details(int id)
+        {
+           
+            List<Contact> list = contact.GetContacts();
+
+            return View(list.Where(item => item.Id == id).First());
+        }
+    }
+}
---
+++ test1/Models/Contact.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace test1.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public int Age { get; set; }
+        private Random rnd = new Random();
+        private List<Contact> contacts;
+
+        public List<Contact> GetContacts()
+        {
+            if (contacts == null)
+            {
+                contacts = new List<Contact> {
+                    new Contact
+                    {
+                        Id = 1,
+                        FirstName = "testF1",
+                        LastName = "testL1",
+                        Age = rnd.Next(1,45)
+                    },
+
+                    new Contact
+                    {
+                        Id = 2,
+                        FirstName = "testF2",
+                        LastName = "testL2",
+                        Age = rnd.Next(1,45)
+                    },
+
+                    new Contact
+                    {
+                        Id = 3,
+                        FirstName = "testF3",
+                        LastName = "testL3",
+                        Age = rnd.Next(1,45)
+                    }
+                };
+
+                return contacts;
+            }
+            else
+            {
+                return contacts;
+            } 
+        }
+    }
+}

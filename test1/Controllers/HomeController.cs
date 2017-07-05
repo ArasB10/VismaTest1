4b0da55371592e892300afebdc7ada9d424f719a
aras.braziunas <aras.braziunas@LT-LIT-SC-0323>
2017-07-05 10:09:57 +0300

    Fix: add external library, scripts moved to js file

--- test1/Controllers/HomeController.cs
+++ test1/Controllers/HomeController.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using test1.Models;
-using test1.Services;
+using MyLibrary.Model;
+using MyLibrary.Data;
 
 namespace test1.Controllers
 {
